4bbe9332d70f45830a841af8c7b18c548c097904
wesley-lewis <lewis.wes.23@gmail.com>
2023-07-03 14:04:40 +0530

    Completed the function definitions

--- pkg/shop/application/products.go
+++ pkg/shop/application/products.go
@@ -16,12 +16,15 @@ type ProductsService struct {
 	readModel productReadModel
 }
 
-func NewProductsService() ProductsService {
-
+func NewProductsService(repo products.Repository, readModel productReadModel) ProductsService {
+	return ProductsService{
+		repo,
+		readModel,
+	}
 }
 
-func (s ProductsService) AllProducts() {
-
+func (s ProductsService) AllProducts() ([]products.Product, error) {
+	return s.readModel.AllProducts()
 }
 
 type AddProductCommand struct {
